3d154d1e3517fe7b7273f706c2fa3dd022e9f81f
luyanjie00436 <18359027950@163.com>
2021-08-14 15:40:38 +0800

    版本1.2.1

--- pages/select-lay/select-lay.vue
+++ pages/select-lay/select-lay.vue
@@ -1,63 +1,65 @@
 <template>
 	<view class="content">
-		<uni-section title="通用写法" type="line"></uni-section>
-		<view style="margin-top: 30rpx;">
-			<luyj-select-lay :zindex="1211" :loading="loading" :value="tval" name="name" placeholder="请选择项目" :options="datalist"
-				@selectitem="selectitem">
-			</luyj-select-lay>
-		</view>
-		
-		<uni-section title="禁用组件" type="line"></uni-section>
-		<view  style="margin-top: 30rpx;">
-			<luyj-select-lay :zindex="1111" :loading="loading" :value="tval" name="name2" placeholder="请选择项目(禁用)" :options="datalist"
-				:disabled="true">
-			</luyj-select-lay>
-		</view>
-		<uni-section title="自定义数据索引对象" type="line"></uni-section>
-		<view style="margin-top: 30rpx;">
-			<luyj-select-lay :loading="loading" :value="tval" name="name3" slabel="myname" svalue="myvalue" placeholder="请选择项目(自定义数据)"
-				:options="datalist2" @selectitem="selectitem2">
-			</luyj-select-lay>
-		</view>
-		<uni-section title="数据加载中" type="line"></uni-section>
-		<view style="margin-top: 30rpx;">
-			<luyj-select-lay :zindex="1211" :loading="true" :value="tval" name="name" placeholder="请选择项目" :options="datalist"
-				@selectitem="selectitem">
-			</luyj-select-lay>
-		</view>
-		<button style="margin: 30rpx;" @click="getDatas">重新加载数据</button>
+		<view class="item ">通用组件：</view>
+		<luyj-select-lay zindex="3" :value="tval" name="name" placeholder="请选择项目" :options="datalist"
+			@selectitem="selectitem">
+		</luyj-select-lay>
+		<view class="item ">组件状态：加载中</view>
+		<luyj-select-lay zindex="3" :value="tval" name="name" placeholder="请选择项目" loading="true" :options="datalist"
+			@selectitem="selectitem">
+		</luyj-select-lay>
+		<view class="item ">禁用组件：</view>
+		<luyj-select-lay zindex="3" :value="tval" name="name2" placeholder="请选择项目(禁用)" :options="datalist"
+			:disabled="true">
+		</luyj-select-lay>
+		<view class="item ">改变下拉框位置：</view>
+		<luyj-select-lay  zindex="3" :value="tval" optionsDirection="top" name="name" placeholder="请选择项目" :options="datalist"
+			@selectitem="selectitem">
+		</luyj-select-lay>
+		<view class="item ">自定义数据索引对象：</view>
+		<luyj-select-lay zindex="3" :value="tval" name="name3" slabel="myname" svalue="myvalue" placeholder="请选择项目(自定义数据)"
+			:options="datalist2" @selectitem="selectitem2">
+		</luyj-select-lay>
+		<!-- <view class="item ">定义为多选:</view>
+		<luyj-select-lay :zindex="3" multiple :value="tval"  name="name" placeholder="请选择项目" :options="datalist"
+			@selectitem="selectitem">
+		</luyj-select-lay> -->
+		<!-- 下一版本添加的说明 -->
+		<!-- |multiple		|Boolean	|false		|	是否多选	| -->
 	</view>
 </template>
 
 <script>
-	import datas from '@/common/data.js'; // 数据
+	import datas from './data.js'; // 数据
 	var _self;
 	export default {
 		data() {
 			return {
-				loading : false,	// 数据是否加载中
 				datalist: [], // 模拟数据
 				datalist2: [], // 模拟自定义数据
 				tval: "" // 模拟初始数据
 			}
 		},
+		onReady:function(){
+			this.datalist = datas.list;
+			this.datalist2 = datas.mylist;
+			this.tval = "value2";
+		},
 		onLoad: function() {
-			_self = this;
-			this.getDatas();
+			self = this;
+			// this.getDatas();
 		},
 		methods: {
 			/**
-			 * 模拟从数据库获取数据(5s)
+			 * 模拟从数据库获取数据
 			 */
-			getDatas: function() {
-				this.loading = true;
-				setTimeout(function() {
-					_self.loading = false;
-					_self.datalist = datas.list;
-					_self.datalist2 = datas.mylist;
-					_self.tval = "value2";
-				}, 5000);
-			},
+			// getDatas: function() {
+			// 	setTimeout(function() {
+			// 		_self.datalist = datas.list;
+			// 		_self.datalist2 = datas.mylist;
+			// 		_self.tval = "value2";
+			// 	}, 3000);
+			// },
 			/** 切换select时执行
 			 * @param {Object} index 索引
 			 * @param {Object} item 值
@@ -92,5 +94,10 @@
 		width: calc(100vw -60rpx);
 		padding: 30rpx;
 		margin: 0 auto;
+	}
+
+	.content .item {
+		margin-top: 20rpx;
+		margin-bottom: 20rpx;
 	}
 </style>
--- uni_modules/luyj-select-lay/readme.md
+++ uni_modules/luyj-select-lay/readme.md
@@ -21,36 +21,91 @@
 
 ``` html
 <!-- 通用写法 -->
-<luyj-select-lay :zindex="1211" :value="tval" name="name" placeholder="请选择项目" :options="datalist"
+<luyj-select-lay :value="tval" name="name" placeholder="请选择项目" :options="datalist"
 	@selectitem="selectitem">
 </luyj-select-lay>
+<!-- 组件状态：加载中 -->
+<luyj-select-lay :value="tval" name="name" placeholder="请选择项目" loading="true" :options="datalist"
+			@selectitem="selectitem">
 <!-- 禁用组件 -->
-<luyj-select-lay :zindex="1111" :value="tval" name="name2" placeholder="请选择项目(禁用)" :options="datalist"
+<luyj-select-lay :value="tval" name="name2" placeholder="请选择项目(禁用)" :options="datalist"
 	:disabled="true">
 </luyj-select-lay>
 <!-- 自定义数据索引对象 -->
 <luyj-select-lay :value="tval" name="name3" slabel="myname" svalue="myvalue" placeholder="请选择项目(自定义数据)"
 	:options="datalist2" @selectitem="selectitem2">
 </luyj-select-lay>
+<!-- 改变下拉框的位置 -->
+<luyj-select-lay :value="tval" optionsDirection="top" name="name" placeholder="请选择项目" :options="datalist"
+			@selectitem="selectitem">
+		</luyj-select-lay>
+```
+
+**实际使用下拉框时，可能需要考虑下列穿透问题**
+
+以上实例，使用的测试数据如下：
+``` javascript
+// data-com 数据
+list: [{
+		label: "label1",
+		value: "value1"
+	},
+	{
+		label: "label2",
+		value: "value2"
+	},
+	{
+		label: "label3",
+		value: "value3"
+	}
+
+],
+// 自定义数据
+mylist: [{
+		myname: "label1",
+		myvalue: "value1"
+	},
+	{
+		myname: "label2",
+		myvalue: "value2"
+	},
+	{
+		myname: "label3",
+		myvalue: "value3"
+	}		
+],
 ```
 
 ### 属性
 
-属性名			|类型		|默认值		|	说明																									
-:-:				|:-:		|:-:		|	:-:	
-disabled		|Boolean	|false		|	是否禁用
-zindex			|Number		|999		|	层级，防止多个组件一起使用时下拉栏穿透
-options			|Array		|[]			|	数据列表
-loading			|Boolean	|false		|	是否加载中
-options			|loadingText|'数据加载中'|	加载时提示文字
-name			|String		|''			|	input字段名
-value			|String		|''			|	默认展示的value值
-placeholder		|String		|''			|	选项值为空时的占位符
-slabel			|String		|'label'	|	自定义列表中键值对应关系（自定义列表的值对应label值）
-svalue			|String		|'value'	|	自定义列表中键值对应关系（自定义列表的值对应value值）
+|属性名			|类型		|默认值		|	说明		|																							
+|:-:			|:-:		|:-:		|	:-:		|
+|disabled		|Boolean	|false		|	是否禁用	|
+|zindex			|Number		|999		|	层级，防止多个组件一起使用时下拉栏穿透	|
+|options		|Array		|[]			|	数据列表	|
+|optionsDirection|String	|bottom		|	数据的位置	|
+|loading		|Boolean	|false		|	是否加载中	|
+|loadingText	|String		|'数据加载中'|	加载时提示文字	|
+|name			|String		|''			|	input字段名	|
+|value			|String		|''			|	默认展示的value值	|
+|placeholder	|String		|''			|	选项值为空时的占位符	|
+|slabel			|String		|'label'	|	自定义列表中键值对应关系（自定义列表的值对应label值）	|
+|svalue			|String		|'value'	|	自定义列表中键值对应关系（自定义列表的值对应value值)	|
+
+#### optionsDirection 默认值
+
+|值			|说明	|
+|:-:		|:-:	|
+|top		|数据列表位于输入框上方	|
+|bottom		|数据列表位于输入框下方	|
 
 ### 事件
 
-事件名			|说明		|返回值																								
-:-:				|:-:		|:-:
-@selectitem		|点击项目触发的事件	|参数（索引，具体项目）
+|事件名			|说明		|返回值	|																								
+|:-:			|:-:		|:-:	|
+|@selectitem	|点击项目触发的事件	|参数（索引，该项的值）	|
+
+## 项目路径
+
+* [codechina](https://codechina.csdn.net/qq_28624235/luyj-select-lay)
+* [github](https://github.com/luyanjie00436/luyj-select-lay)
